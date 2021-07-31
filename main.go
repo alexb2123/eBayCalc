034841fef2d7af604c56a000f33ef37bcc7c391f
alex bordei <alexb2123@gmail.com>
2021-07-31 12:07:51 -0400

    updated vars and formula

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, world.")
-}
+	var sell_cost float64
+	var advert_rate float64	
 
+	var output =((((sell_cost*0.1)+sell_cost)*(0.1))+(sell_cost * 0.029)+0.3)+(sell_cost * advert_rate)
+}
