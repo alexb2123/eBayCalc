a04cfa3394ecb4d8f2292a738a3ee3bb26ae795d
alex bordei <alexb2123@gmail.com>
2021-08-05 10:43:24 -0400

    just testing sme stuff

--- main.go
+++ main.go
@@ -1,8 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+// some functions
+func calculateFees(W http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+}
+
 
 func main() {
+
+
+// Router and Handler
+	r := mux.NewRouter()
+	r.HandleFunc("/calculate", calculateFees).Methods("GET")
+
+
+// 
+	
 	var sell_cost float64
 	var advert_rate float64	
 
@@ -15,5 +37,9 @@ func main() {
 
 	var output =((((sell_cost*0.1)+sell_cost)*(0.1))+(sell_cost * 0.029)+0.3)+(sell_cost * advert_rate)
 
+	
+
 	fmt.Printf("%f %f = %f", sell_cost, advert_rate, output)
+
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
